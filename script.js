905811148b8e9242f032959265fd14a440dc6e24
Michal Michalik <michal.michalik@gmail.com>
2018-01-12 12:19:17 +0100

    poprawka link do heroku

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://forestfart-crud-app.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://forestfart-crud-app.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
